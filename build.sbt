7891a1e8b586499703b0a2b8d9fa686e935b9ba2
Ali Baig <abaig28@uic.edu>
2020-09-11 18:37:41 -0500

    Added Logback & Typesafe Config Libraries

--- build.sbt
+++ build.sbt
@@ -3,3 +3,8 @@ name := "HW1_Solution"
 version := "0.1"
 
 scalaVersion := "2.12.7"
+
+libraryDependencies ++= Seq(
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
+  "com.typesafe" % "config" % "1.4.0",
+)
--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,5 +1,12 @@
-object Hello {
-  def main(args: Array[String]): Unit = {
-    println("Testing Git with HW1 again")
-  }
+import com.typesafe.config.{Config, ConfigFactory}
+import org.slf4j.LoggerFactory
+
+object Hello extends App {
+
+  // Logback SLF4J for logging debug, info, and warn statements
+  val logger = LoggerFactory.getLogger("Hello")
+  // Load Typesafe Configuration Library for input
+  val config : Config = ConfigFactory.load("lightbend.conf")
+
+  logger.info("Hello, my name is " + config.getString("conf.name"))
 }
